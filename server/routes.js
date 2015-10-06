96b74165b272abc3d212565d04e84b82dd33c541
Mike <michalpso@gmail.com>
2015-10-06 13:14:39 -0700

    refactored routes out of server.js

---
+++ server/routes.js
@@ -0,0 +1,36 @@
+var router = require('express').Router();
+var app = require('./server.js')
+var auth = require('./auth/auth');
+var utils = require('./lib/utils');
+
+// Authentication Routes
+router.post('/signup', function(req, res) {
+  auth.signup(req.body.username, req.body.password)
+  .then(function(result) {
+    res.status(201)
+    .send(result);
+  })
+  .catch(function(err) {
+    res.status(300)
+    .send(err);
+  });
+});
+
+router.post('/login', function(req, res) {
+  auth.login(req.body.username, req.body.password)
+  .then(function(user) {
+    utils.createSession(req, res, user, function() {
+      res.status(200).send(user);
+    });
+  })
+  .catch(function(err) {
+    res.status(300)
+    .send(err);
+  });
+});
+
+router.post('/logout', utils.destroySession, function(req, res) {
+  res.status(200).end();
+});
+
+module.exports = router;
