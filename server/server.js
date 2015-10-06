1bf2fdf24e770a8e0e522ebe4bd762af4458a9ea
Mike <michalpso@gmail.com>
2015-10-06 12:11:33 -0700

    refactored server listen to console log port and handle errors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-server.listen(require('./config.js').port);
+var port = require('./config.js').port
+
 
 //config dependencies
 var secret = require('./secret.js');
@@ -20,17 +21,27 @@ var matchCtrl = require('./match/matchController');
 var chatCtrl = require('./chat/chatController');
 var utils = require('./lib/utils');
 
+server.listen(port,function(err){
+  if (err){
+    console.log('unable to listen ',err)
+  } else {
+    console.log('listening on port ',port)
+  }
+});
+
 if( (process.env.NODE_ENV === 'development') || !(process.env.NODE_ENV) ){
   app.use(logger('dev'));
 }
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use(session({
   secret: secret,
   resave: true,
   saveUninitialized: true
 }));
+
 app.use("/", express.static(__dirname + '/../client-web'));
 
 // Sockets Connection
@@ -75,27 +86,27 @@ io.of('/chat').on('connection', function (socket) {
 // Authentication Routes
 app.post('/signup', function(req, res) {
   auth.signup(req.body.username, req.body.password)
-    .then(function(result) {
-      res.status(201)
-        .send(result);
-    })
-    .catch(function(err) {
-      res.status(300)
-        .send(err);
-    });
+  .then(function(result) {
+    res.status(201)
+    .send(result);
+  })
+  .catch(function(err) {
+    res.status(300)
+    .send(err);
+  });
 });
 
 app.post('/login', function(req, res) {
   auth.login(req.body.username, req.body.password)
-    .then(function(user) {
-      utils.createSession(req, res, user, function() {
-        res.status(200).send(user);
-      });
-    })
-    .catch(function(err) {
-      res.status(300)
-        .send(err);
+  .then(function(user) {
+    utils.createSession(req, res, user, function() {
+      res.status(200).send(user);
     });
+  })
+  .catch(function(err) {
+    res.status(300)
+    .send(err);
+  });
 });
 
 app.post('/logout', utils.destroySession, function(req, res) {
