c81faff16f4d0dd800cdc4034592ba00c95d6663
camhux <camzig@gmail.com>
2015-10-06 18:59:43 -0700

    Add state transition to finder controller

--- client-web/finder/finder.js
+++ client-web/finder/finder.js
@@ -1,6 +1,6 @@
 angular.module('kwiki.finder', ['services.socket', 'services.user'])
 
-.controller('FinderController', ['$scope', 'Socket', 'User', function ($scope, Socket, User) {
+.controller('FinderController', ['$scope', '$state', 'Socket', 'User', function ($scope, $state, Socket, User) {
   $scope.disableButton = false;
 
   $scope.address = '';
@@ -10,6 +10,7 @@ angular.module('kwiki.finder', ['services.socket', 'services.user'])
     $scope.disableButton = true;
     Socket.emit('joinRoom', {username: User.current(), address: $scope.address});
     // TODO: state transition
+    $state.transitionTo('chat');
   };
 
   $scope.logOut = function () {
