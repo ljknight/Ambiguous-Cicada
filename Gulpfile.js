914b54370fcf6c7bcf3b74c023a3e975683ee145
camhux <camzig@gmail.com>
2015-10-07 15:10:20 -0700

    Add gulp-watching

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var cache = require('gulp-cached');
+var remember = require('gulp-remember');
 var karma = require('karma');
 
 var clientDepPaths = [
@@ -26,6 +28,7 @@ gulp.task('karma', function(done) {
 gulp.task('build-dev', function() {
   return gulp.src( clientDepPaths.concat( 'client-web/app.js', 'client-web/!(build|tests)/*.js' ) , {base: '.'} )
     .pipe(sourcemaps.init())
+
     .pipe(concat('build.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('client-web/build/'));
@@ -34,8 +37,30 @@ gulp.task('build-dev', function() {
 gulp.task('build', function() {
   return gulp.src( clientMinDepPaths.concat( 'client-web/app.js', 'client-web/!(build|tests)/*.js' ) , {base: '.'} )
     .pipe(sourcemaps.init())
-    .pipe(concat('build.js'))
+    .pipe(cache('build'))
     .pipe(uglify())
+    .pipe(remember('build'))
+    .pipe(concat('build.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('client-web/build/'));
+});
+
+gulp.task('watch', function() {
+  var watcher = gulp.watch(['client-web/app.js', 'client-web/!(build|tests)/*.js'], ['build']);
+  watcher.on('change', function(event) {
+    if (event.type === 'deleted') {                   // if a file is deleted, forget about it
+          delete cached.caches.build[event.path];       // gulp-cached remove api
+          remember.forget('build', event.path);         // gulp-remember remove api
+        }
+  });
+});
+
+gulp.task('watch-dev', function() {
+  var watcher = gulp.watch(['client-web/app.js', 'client-web/!(build|tests)/*.js'], ['build-dev']);
+  watcher.on('change', function(event) {
+    if (event.type === 'deleted') {                   // if a file is deleted, forget about it
+          delete cached.caches.build[event.path];       // gulp-cached remove api
+          remember.forget('build', event.path);         // gulp-remember remove api
+        }
+  });
 });
