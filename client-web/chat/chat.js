286bf12e9d9d07cb298ca11fe1b53b0425af8da1
Laura Knight <ljknight@gmail.com>
2015-10-08 11:04:16 -0700

    Add nav bar; remove nav functionality on chat view

--- client-web/chat/chat.js
+++ client-web/chat/chat.js
@@ -20,44 +20,41 @@ angular.module('kwiki.chat', ['services.socket', 'services.user'])
 //   return chatFact;
 // }])
 
-.controller('ChatController', ['$scope', '$rootScope', 'Socket', 'User', function ($scope, $rootScope, Socket, User) {
+.controller('ChatController', ['$scope', 'Socket', 'User',
+  function($scope, Socket, User) {
 
-  $scope.chatMessages = [];
+    $scope.chatMessages = [];
 
-  Socket.on('chatMessage', function(text) {
-    console.log(text)
-    $scope.chatMessages.unshift(message(text));
-    $scope.$apply();
-  });
+    Socket.on('chatMessage', function(text) {
+      console.log(text);
+      $scope.chatMessages.unshift(message(text));
+      $scope.$apply();
+    });
 
-  var message = function(text) {
-    return {
-      user:User.current(),
-      text:text.toString(),
-      humanTime:moment(new Date()).fromNow(),
-      createdAt: new Date()
-    } 
-  } 
-
-  setInterval(function(){
-    for (var i = 0; i < $scope.chatMessages.length; i++) {
-      console.log($scope.chatMessages[i].humanTime);
-      $scope.chatMessages[i].humanTime = moment($scope.chatMessages[i].createdAt).fromNow()
+    var message = function(text) {
+      return {
+        user: User.current(),
+        text: text.toString(),
+        humanTime: moment(new Date()).fromNow(),
+        createdAt: new Date()
+      };
     };
-    $scope.$apply();
-  },30*Math.pow(10,3))
 
-  $scope.sendMessage = function () {
-    if ( $scope.messageInput ) {
-      Socket.emit('sendMessage', $scope.messageInput);
-      $scope.chatMessages.unshift(message($scope.messageInput));
-      $scope.messageInput = '';
-    }
-  };
+    setInterval(function() {
+      for (var i = 0; i < $scope.chatMessages.length; i++) {
+        console.log($scope.chatMessages[i].humanTime);
+        $scope.chatMessages[i].humanTime = moment($scope.chatMessages[i].createdAt).fromNow();
+      }
+      $scope.$apply();
+    }, 30 * Math.pow(10, 3));
 
-  $scope.logOut = function () {
-    User.logOut();
-  };
+    $scope.sendMessage = function() {
+      if ($scope.messageInput) {
+        Socket.emit('sendMessage', $scope.messageInput);
+        $scope.chatMessages.unshift(message($scope.messageInput));
+        $scope.messageInput = '';
+      }
+    };
 
-}]);
-
+  }
+]);
