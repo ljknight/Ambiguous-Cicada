d68e00a10ef9cc29facb178b82cad7c5e2a0c6ca
camhux <camzig@gmail.com>
2015-10-08 14:46:29 -0700

    Add spinner to waiting screen

--- client-web/waiting/waiting.js
+++ client-web/waiting/waiting.js
@@ -1,6 +1,8 @@
 angular.module('kwiki.waiting', ['services.spinner', 'services.socket'])
   .controller('WaitingController', ['$scope', '$state', 'Spinner', 'Socket', 
     function($scope, $state, Spinner, Socket) {
+      var spinner = Spinner.create();
+      spinner.spin(document.querySelector('.spinner'));
       $scope.cancel = function() {
         Socket.emit('cancelLucky');
         $state.transitionTo('finder');
